483a51dca6cd3957cb0182d6da9cd96a88516574
Mark Fisher <mkfisher82@gmail.com>
2018-10-25 14:25:12 +1300

    Added facebook opengraph to head

--- src/components/layout.js
+++ src/components/layout.js
@@ -37,6 +37,17 @@ const Layout = ({ children }) => (
             { name: 'keywords', content: 'running, wellbeing, mental health' },
             { name: 'viewport', content: 'width=device-width' },
             { name: 'google-site-verification', content: 'UeaKXdCgblOxP54PXFqr4j8OpPAAuJglUKtJ9UrOfVM' },
+            // Facebook Opengraph
+            { name: 'og:url', content: 'https://www.run2wellbeing.co.nz'},
+            { name: 'og:type', content: 'website'},
+            { name: 'og:title', content: 'Run 2 Wellbeing'},
+            { name: 'og:description', content: 'Fitness coaching for mind and body'},
+            { name: 'og:image', content: 'https://www.run2wellbeing.co.nz/static/kalen-emsley-99660-unsplash-c_scale,w_1920-a2f4a979bb92c202a0d72fb5c04889ac.jpeg'},
+            { name: 'og:image:alt', content: 'Running hills as the sunrises'},
+            { name: 'og:image:type', content: 'image/jpeg'},
+            { name: 'og:image:width', content: '1920'},
+            { name: 'og:image:height', content: '700'},
+
           ]}
           link={[
             {rel: 'shortcut icon', type: 'image/png', href: `${Favicon}`}
